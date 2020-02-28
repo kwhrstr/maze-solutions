88e883d88be0cd2a5b31f5744eaeeab1c1f01bc3
kawaharashotaro <kkawaharasshoutarou@gmail.com>
2020-02-29 03:23:09 +0900

    maze-h修正
    Alternativeに修正

--- maze-h/src/Lib.hs
+++ maze-h/src/Lib.hs
@@ -1,9 +1,9 @@
 module Lib where
 
 import Data.Maybe (listToMaybe)
-import Control.Monad
 import Data.List
 import Data.Function (on)
+import Control.Applicative (Alternative(..), (<|>))
 
 type Cell = (Int, Int)
 type Maze = [(Cell, Char)]
@@ -19,17 +19,17 @@ nextSteps (x, y) mz =
       [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
   
 parseMaze :: [String] -> Maze
-parseMaze = 
- concatMap (\(y, str) -> zipWith (\x ch -> ((x, y), ch)) [0..] str) . zip [0 ..] 
+parseMaze =
+ concatMap (\(y, str) -> zipWith (\x ch -> ((x, y), ch)) [0..] str) . zip [0 ..]
 
 firstPath :: ([Path] -> Cell-> Maze -> Maybe Path)
                -> Cell -> Cell -> Maze -> Maybe Path
 firstPath f p0 = f [[p0]]
 
-_byB :: MonadPlus m => [Path] -> Cell -> Maze -> m Path
-_byB [] _ _ = mzero
+_byB :: Alternative m => [Path] -> Cell -> Maze -> m Path
+_byB [] _ _ = empty
 _byB ap@(path:queue) p1 mz
-  | head path == p1 = return (reverse path) `mplus` _byB  queue p1 mz
+  | head path == p1 = pure path <|> _byB  queue p1 mz
   | otherwise =
     _byB (queue ++ [x : path | x <- nextSteps (head path) mz, all (x `notElem`) ap]) p1 mz
 
@@ -43,10 +43,10 @@ solve ls = do
   st <- listToMaybe $ findPoints 'S' mz
   gl <- listToMaybe $ findPoints 'G' mz
   path <- firstPath _byB st gl mz
-  let f c ch = if c `elem` path && ch == ' ' 
-               then '$' else ch  
+  let f c ch = if c `elem` path && ch == ' '
+               then '$' else ch
   let solvedMaze = map (\(c, ch) -> (c, f c ch)) mz
-  let ans = transpose . map (map snd) 
+  let ans = transpose . map (map snd)
           . groupBy ((==) `on` fst . fst) . sortOn fst $ solvedMaze
   return ans
 
